1f08f3ba5ee41620f9ae106d93edf369e1621a68
start.vaadin.com <no-reply@vaadin.com>
2022-02-11 19:04:22 +0000

    Generated project

---
+++ src/main/java/com/example/application/views/exercise3/Exercise3View.java
@@ -0,0 +1,31 @@
+package com.example.application.views.exercise3;
+
+import com.example.application.views.MainLayout;
+import com.vaadin.flow.component.html.H2;
+import com.vaadin.flow.component.html.Image;
+import com.vaadin.flow.component.html.Paragraph;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+
+@PageTitle("Exercise 3")
+@Route(value = "Exercise-3", layout = MainLayout.class)
+public class Exercise3View extends VerticalLayout {
+
+    public Exercise3View() {
+        setSpacing(false);
+
+        Image img = new Image("images/empty-plant.png", "placeholder plant");
+        img.setWidth("200px");
+        add(img);
+
+        add(new H2("This place intentionally left empty"));
+        add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+        setSizeFull();
+        setJustifyContentMode(JustifyContentMode.CENTER);
+        setDefaultHorizontalComponentAlignment(Alignment.CENTER);
+        getStyle().set("text-align", "center");
+    }
+
+}
